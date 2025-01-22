6ac4aef11142b037309b17d1ad3612344135b4e5
Fahime <nafisurr93@gmail.comm>
2025-01-23 01:37:31 +0600

    demo

--- src/components/product/ProductDetails.jsx
+++ src/components/product/ProductDetails.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import Container from '../Container'
+import { Link } from 'react-router-dom'
+import Product from '../../assets/products1.png'
+import { IoMdStar, IoMdStarHalf, IoMdStarOutline } from "react-icons/io";
+// import axios from 'axios';
 
 const ProductDetails = () => {
+
+  // let [singleProduct, setSingleProduct] = useState({})
+  // let singleData = () => {
+  //   axios.get('https://dummyjson.com/products?sortBy=title&order=asc').then((response) => {
+  //     setSingleProduct(response.data);
+  //   })
+  // }  
+
+  // useEffect(() => {
+  //   singleData()
+  // }, [])
+
   return (
-    <div>ProductDetails</div>
+    <section>
+      <Container>
+        <nav class="flex py-[10px]" aria-label="Breadcrumb">
+          <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center">
+              <Link to="/" href="#" class="inline-flex items-center text-[18px] font-medium text-[#767676] hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                Home
+              </Link>
+            </li>
+            <li>
+              <div class="flex items-center">
+                <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                </svg>
+                <Link to="/shop" href="#" class="ms-1 text-[18px] font-medium text-[#767676] hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Products</Link>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="flex justify-center py-[60px]">
+          <div className="w-[40%]">
+            <img src={Product} alt="" />
+          </div>
+
+          <div className="w-[40%]">
+            <h3 className='font-dm'>Products</h3>
+            <div className="text-[20px] flex items-center gap-1">
+              <IoMdStar />
+              <IoMdStarHalf />
+              <IoMdStarOutline />
+              <span className='font-dm font-normal text-[#767676] text-[14px]'>1 Review</span>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import Container from '../components/Container'
-import { IoIosArrowForward } from "react-icons/io";
 import { FaCodeCompare, FaHeart, FaPlus } from "react-icons/fa6";
 import { IoMdArrowDropup } from "react-icons/io";
 import { ApiData } from '../components/ContextApi';
@@ -15,7 +14,6 @@ const Shop = () => {
       <Container>
         <div className="">
           <h3 className="font-dm font-bold text-[#262626] text-[49px]">Products</h3>
-          <p className="font-dm font-normal text-[12px] text-[#767676] flex items-center gap-1">Home <IoIosArrowForward /> Products</p>
         </div>
 
         <div className="py-[130px] flex justify-between">
@@ -93,17 +91,19 @@ const Shop = () => {
                   <div className="relative group overflow-y-hidden bg-[#efefef]">
                     <div className="relative pb-[20px]">
                       <Link to={`${item.id}`}>
-                      <img className='w-[100%]' src={item.thumbnail} alt="" />
+                        <img className='w-[100%]' src={item.thumbnail} alt="" />
                       </Link>
                       <p className='absolute top-[20px] left-[20px] py-[9px] px-[30px] bg-[#262626] text-[#fff] font-dm font-bold text-[14px] inline-block'>New</p>
                     </div>
                     <div className="absolute bottom-0 left-0 w-full h-[0] opacity-0 bg-[#fff] group-hover:h-[150px] group-hover:opacity-100 duration-300 ease-in-out">
                       <ul className='flex justify-end items-center h-full'>
-                        <div className="pr-[30px]">
-                          <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-normal text-[16px] text-[#767676] cursor-pointer'>Add to Wish List <FaHeart /></li>
-                          <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-normal text-[16px] text-[#767676] cursor-pointer'>compare <FaCodeCompare /></li>
-                          <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-bold text-[16px] text-[#262626] cursor-pointer'>Add to Cart <FaShoppingCart /></li>
-                        </div>
+                        <Link to={`${item.id}`}>
+                          <div className="pr-[30px]">
+                            <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-normal text-[16px] text-[#767676] cursor-pointer'>Add to Wish List <FaHeart /></li>
+                            <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-normal text-[16px] text-[#767676] cursor-pointer'>compare <FaCodeCompare /></li>
+                            <li className='flex gap-x-[15px] items-center justify-end py-1 font-dm font-bold text-[16px] text-[#262626] cursor-pointer'>Add to Cart <FaShoppingCart /></li>
+                          </div>
+                        </Link>
                       </ul>
                     </div>
                   </div>
